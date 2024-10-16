d98c1191dee895ddbe92bce43c594b4c4e86b0ba
Leguminous <murayaleo911@gmail.com>
2024-10-16 11:54:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){  
+     return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("Dog racing");
+
+function mondayWork(what = "go to the office"){
+    return `This Monday, I will ${what}.`;
+}
+mondayWork();
+mondayWork("be at the site");
+
+// function wrapAdjective(defaultText = "*"){
+    
+//     return `You are ${defaultText}`
+// }
+
+// wrapAdjective('');
+
+function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
